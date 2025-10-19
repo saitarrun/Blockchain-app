8b690d8469edf95f7654fc334e7aba3e94052423
sai <s@gmail.comgit config --global user.name saigit config --global user.name sai1>
2025-10-18 19:55:38 -0700

    phase2

---
+++ images/images.js
@@ -0,0 +1,22 @@
+/** Local art pool used by the arena cards.
+ *  Add/remove filenames here to change the pool.
+ */
+window.ZOMBIE_IMAGES = [
+  "cartoon-blue-zombie-head-vector-26684274.avif",
+  "cartoon-zombie-graveyard-vector-790116.avif",
+  "cartoon-zombie-head-vector-21359926.avif",
+  "cartoon-zombie-head-vector-22609051.avif",
+  "cartoon-zombie-vector-2138255.avif",
+  "cartoon-zombie-vector-22441529.avif",
+  "cute-zombie-cartoon-vector-1693430.avif",
+  "green-goblin-monster-vector-134349.avif",
+  "melting-skull-smiley-horror-cartoon-vector-46480203.avif",
+  "spooky-halloween-zombie-cartoon-vector-1585348.avif",
+  "zombie-cartoon-set-halloween-theme-vector-6015006.avif",
+  "zombie-halloween-cartoon-vector-27138078.avif",
+  "zombie-christmas-cartoon-vector-49483160.avif",
+  "zombie-dance-halloween-vector-27489333.avif",
+  "zombie-mascot-logo-vector-26837403.avif",
+  "zombie-office-workers-cartoon-vector-27289340.avif",
+  "spooky-halloween-zombie-vector-27489333.avif"
+];
