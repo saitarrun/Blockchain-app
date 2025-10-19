d5e56dc948a487ee5b1cedd2660db069b94c9d0e
sai <s@gmail.comgit config --global user.name saigit config --global user.name sai1>
2025-10-19 03:25:52 -0700

    Final Modifications Need to add Readme.md file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,7 @@
-const SafeMath = artifacts.require("./safemath.sol");
-const ZombieFactory = artifacts.require("./zombiefactory.sol");
-const ZombieFeeding = artifacts.require("./zombiefeeding.sol");
-const ZombieHelper = artifacts.require("./zombiehelper.sol");
-const ZombieAttack = artifacts.require("./zombieattack.sol");
-const ZombieOwnership = artifacts.require("./zombieownership.sol");
+const CryptoZombies = artifacts.require("./CryptoZombies.sol");
 const Marketplace = artifacts.require("./marketplace.sol");
 
 module.exports = async function(deployer) {
-  await deployer.deploy(SafeMath);
-  await deployer.link(SafeMath, [ZombieFactory, ZombieFeeding, ZombieHelper, ZombieAttack, ZombieOwnership]);
-  await deployer.deploy(ZombieFactory);
-  await deployer.deploy(ZombieFeeding);
-  await deployer.deploy(ZombieHelper);
-  await deployer.deploy(ZombieAttack);
-  const zombieOwnership = await deployer.deploy(ZombieOwnership);
-  await deployer.deploy(Marketplace, zombieOwnership.address);
+  await deployer.deploy(CryptoZombies);
+  await deployer.deploy(Marketplace, CryptoZombies.address);
 };
